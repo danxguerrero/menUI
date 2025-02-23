c8863e640e97688cd4e50d1371965e853d4258b7
danxguerrero <danxguerrero@yahoo.com>
2025-02-22 23:25:27 -0500

    User can create a restaurant. closes #5

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import { Authenticated, Unauthenticated } from 'convex/react' 
 import { MenuBar } from './components/MenuBar.tsx'
-import { Button } from '@mui/material';
+import { Button, TextField } from '@mui/material';
 import { useMutation } from "convex/react";
 import { api } from "../convex/_generated/api"; 
 import { useStoreUserEffect } from './useStoreUserEffect';
+import { useState } from 'react';
+
 function App() {
   const createRestaurant = useMutation(api.functions.addRestaurant.createRestaurant);
+  const [isCreatingRestauraunt, setIsCreatingRestaurant] = useState(false);
+  const [restaurantName, setRestaurantName] = useState("")
   useStoreUserEffect();
   return (
     <main>
@@ -14,8 +18,17 @@ function App() {
         <p>Please sign in to continue</p>
       </Unauthenticated>
       <Authenticated>
-        <p>Welcome to MenUI</p> 
-        <Button onClick={() => createRestaurant({ name: "Test Restaurant" })}>Create Restaurant</Button>
+        <p>Welcome to MenUI</p>
+        
+        {isCreatingRestauraunt ? (
+          <>
+            <TextField value={restaurantName} onChange={(e)=> setRestaurantName(e.target.value)}/>
+            <Button onClick={() => {
+              createRestaurant({ name: restaurantName })
+              setIsCreatingRestaurant(false)
+            }}>Create</Button>
+          </>) : <Button onClick={()=> setIsCreatingRestaurant(!isCreatingRestauraunt)}>Create Restaurant</Button>
+        }
       </Authenticated>
     </main>
   )
